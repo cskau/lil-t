1fb18f73bed775dee107d285342cb8e3744e7bb6
Clement Skau <cskau+github@gmail.com>
2017-02-26 17:51:05 +0900

    More robust demo script.

--- demo.sh
+++ demo.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 add_plugin () {
+  sudo -u pi -g audio jack_disconnect "a2j:Teensy MIDI [20] (playback): Teensy MIDI MIDI 1" "effect_0:$2"
+  sudo -u pi -g audio jack_disconnect "effect_0:$3" "system:playback_1"
+  sudo -u pi -g audio jack_disconnect "effect_0:$4" "system:playback_2"
+
+  nc -w1 localhost 5556
+  echo -n "remove 0" | nc -w1 localhost 5555
+  nc -w1 localhost 5556
+
   nc -w1 localhost 5556
   echo -n "add $1 0" | nc -w1 localhost 5555
   nc -w1 localhost 5556
 
-  sudo jack_connect "a2j:Teensy MIDI [20] (playback): Teensy MIDI MIDI 1" "effect_0:$2"
-  sudo jack_connect "effect_0:$3" "system:playback_1"
-  sudo jack_connect "effect_0:$4" "system:playback_2"
+  sudo -u pi -g audio jack_connect "a2j:Teensy MIDI [20] (playback): Teensy MIDI MIDI 1" "effect_0:$2"
+  sudo -u pi -g audio jack_connect "effect_0:$3" "system:playback_1"
+  sudo -u pi -g audio jack_connect "effect_0:$4" "system:playback_2"
 }
 
 case $((RANDOM % 5)) in
